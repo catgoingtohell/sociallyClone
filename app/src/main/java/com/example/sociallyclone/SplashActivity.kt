4092fc831d3426631dbc984c9ae4658be6ee2994
catgoingtohell <king722.k8@gmail.com>
2025-10-27 15:19:24 +0500

    remember login layout made

--- app/src/main/java/com/example/sociallyclone/SplashActivity.kt
+++ app/src/main/java/com/example/sociallyclone/SplashActivity.kt
@@ -1,20 +1,19 @@
 package com.example.sociallyclone
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-
+import android.content.Intent
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
 
-        // Delay for 2 seconds (adjust as needed)
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, SignupActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000)
