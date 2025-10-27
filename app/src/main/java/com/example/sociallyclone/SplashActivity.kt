c08dc1abee387f7f2b36d5e9c3f953df836f5664
catgoingtohell <king722.k8@gmail.com>
2025-10-27 13:04:52 +0500

    splash screen ok

---
+++ app/src/main/java/com/example/sociallyclone/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.sociallyclone
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Delay for 2 seconds (adjust as needed)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Close splash activity
+        }, 10000)
+    }
+}
